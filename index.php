21336c89497c35755e72c388b36d7f2426cd83bc
SniffRx <37187657+SniffRx@users.noreply.github.com>
2022-03-31 16:22:28 +0300

    Update index.php
    Added copyright

--- index.php
+++ index.php
@@ -87,6 +87,15 @@ if(file_exists($user1) && file_exists($user2) && (time() - filemtime($user1) < $
     </div>
     <!--JavaScript at end of body for optimized loading-->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <footer><div class="copyright" align="center">
+  <script>
+    document.write('&copy;' );
+    document.write(' 2022 - ');
+    document.write(new Date().getFullYear());
+    document.write(' <a href="https://github.com/SniffRx/">SniffRx</a> - All Rights Reserved.');
+    document.write('<br/>Last Updated : ');
+    document.write(document.lastModified);
+  </script></footer>
 </body>
 </html>
 <script>
